28e4a7a6580dbbf6bb26930408bccd3c61b84e1e
RawIron <manianology@gmail.com>
2017-03-23 13:36:00 +0100

    added implementation of itoa() for a quick test

---
+++ lib/itoa.cpp
@@ -0,0 +1,40 @@
+/**
+ * http://www.strudel.org.uk/itoa/
+ *
+ * C++ version 0.4 char* style "itoa":
+ * Written by Lukás Chmela
+ * Released under GPLv3.
+ */
+#include <iostream>
+#include <assert.h>
+
+char* itoa(int value, char* result, int base) {
+	if (base < 2 || base > 36) { *result = '\0'; return result; }
+
+	char *ptr = result, *ptr1 = result, tmp_char;
+	int tmp_value;
+
+	do {
+		tmp_value = value;
+		value /= base;
+		*ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+	} while ( value );
+
+	if (tmp_value < 0) *ptr++ = '-';
+
+	*ptr-- = '\0';
+	while(ptr1 < ptr) {
+		tmp_char = *ptr;
+		*ptr-- = *ptr1;
+		*ptr1++ = tmp_char;
+	}
+
+	return result;
+}
+
+
+int main() {
+    char *number = new char[35];
+    itoa(234, number, 2);
+    std::cout << number << std::endl;
+}
