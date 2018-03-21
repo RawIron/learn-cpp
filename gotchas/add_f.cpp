6adb9311101afdbab46503c4df4ece7c5b880ff6
RawIron <manianology@gmail.com>
2018-03-21 08:34:53 -0700

    Add 1 test

--- gotchas/add_f.cpp
+++ gotchas/add_f.cpp
@@ -1,3 +1,6 @@
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
 #include <iostream>
 
 int f(int);
@@ -16,10 +19,12 @@ double add_f(double p) {
     return p+(f(1)+f(1.0));
 }
 
+BOOST_AUTO_TEST_SUITE(overloading)
 
-int main() {
-    double a;
-    a = add_f(100.5);
-    std::cout << a << std::endl;
+BOOST_AUTO_TEST_CASE( test_is_double )
+{
+    double a = 104.8;
+    BOOST_TEST(a == add_f(100.5));
 }
 
+BOOST_AUTO_TEST_SUITE_END()
