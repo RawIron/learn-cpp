aed827bf89773e8e93961fe2719dd382a48f2f91
RawIron <manianology@gmail.com>
2018-03-25 12:41:55 -0700

    Add replay method; bit of cleaning

--- gotchas/inheritance.cpp
+++ gotchas/inheritance.cpp
@@ -2,7 +2,9 @@
 #include <boost/test/unit_test.hpp>
 
 #include <string>
+#include <vector>
 #include <iostream>
+#include <cstddef>
 
 typedef std::string state_t;
 typedef std::string event_t;
@@ -16,8 +18,8 @@ class cleaner_interface {
 class cleaner : public cleaner_interface {
 public:
     cleaner() : state_("idle") {};
-    virtual state_t clean(std::string event);
-    std::string print() const;
+    virtual ~cleaner() {};
+    virtual state_t clean(event_t event);
 private:
     static const std::string name_;
     state_t state_;
@@ -25,49 +27,53 @@ private:
 
 const std::string cleaner::name_ = "cleaner";
 
-state_t cleaner::clean(std::string event) {
+state_t cleaner::clean(event_t event) {
     state_ = event;
     return state_;
-}
-
-std::string cleaner::print() const {
-    return "base class";
 }
 
 
 class cleaner_buy : public cleaner {
 public:
-    cleaner_buy() : event("{}"), state("idle") {};
+    cleaner_buy() : event_history(), state("idle_buyer") {};
+    virtual ~cleaner_buy() {};
     virtual state_t clean(event_t event);
-    std::string print() const;
+    virtual state_t replay(state_t using_state, std::size_t from_pos);
+    std::string to_str() const;
 private:
-    std::string event;
+    std::vector<event_t> event_history;
+    std::vector<event_t>::const_iterator c_it;
     state_t state;
 };
 
 state_t cleaner_buy::clean(event_t event) {
-    (*this).event = event;
+    event_history.push_back(event);
     return state;
 }
 
-event_t cleaner_buy::print() const {
-    return event;
+state_t cleaner_buy::replay(state_t using_state, std::size_t from_pos) {
+    c_it = event_history.cbegin() + from_pos;
+    return using_state;
+}
+
+std::string cleaner_buy::to_str() const {
+    return state;
 }
 
 
 BOOST_AUTO_TEST_SUITE(inheritance)
 
-BOOST_AUTO_TEST_CASE(virtual_override) {
-    const event_t event = R"<<<({"name": "buy"})<<<";
+BOOST_AUTO_TEST_CASE(call_virtual_override) {
+    const event_t event = R"<<({"name": "buy"})<<";
     cleaner_buy cb;
-    BOOST_TEST("idle" == cb.clean(event));
+    BOOST_TEST("idle_buyer" == cb.clean(event));
 }
 
-BOOST_AUTO_TEST_CASE(virtual_override_on_base) {
-    const event_t event = R"<<<({"name": "sell"})<<<";
+BOOST_AUTO_TEST_CASE(call_virtual_override_on_base) {
+    const event_t event = R"<<({"name": "sell"})<<";
     cleaner_buy cb;
     cleaner* cp = &cb;
-    BOOST_TEST("idle" == cp->clean(event));
+    BOOST_TEST("idle_buyer" == cp->clean(event));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
