169158f4aa17d8a6cbbda03b3a39ddd908f684c6
RawIron <manianology@gmail.com>
2018-03-24 20:46:39 -0700

    2 test for inheritance

--- gotchas/inheritance.cpp
+++ gotchas/inheritance.cpp
@@ -1,52 +1,73 @@
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
 #include <string>
 #include <iostream>
 
+typedef std::string state_t;
+typedef std::string event_t;
+
 
 class cleaner_interface {
-    virtual bool clean(std::string event) = 0;
+    virtual state_t clean(event_t event) = 0;
 };
 
 
 class cleaner : public cleaner_interface {
-    public:
-    virtual bool clean(std::string event);
-    std::string print();
+public:
+    cleaner() : state_("idle") {};
+    virtual state_t clean(std::string event);
+    std::string print() const;
+private:
+    static const std::string name_;
+    state_t state_;
 };
 
-bool cleaner::clean(std::string data) {
-    return "cleaned";
+const std::string cleaner::name_ = "cleaner";
+
+state_t cleaner::clean(std::string event) {
+    state_ = event;
+    return state_;
 }
 
-std::string cleaner::print() {
+std::string cleaner::print() const {
     return "base class";
 }
 
 
 class cleaner_buy : public cleaner {
-    public:
-    virtual bool clean(std::string event);
-    std::string print();
-    private:
+public:
+    cleaner_buy() : event("{}"), state("idle") {};
+    virtual state_t clean(event_t event);
+    std::string print() const;
+private:
     std::string event;
+    state_t state;
 };
 
-bool cleaner_buy::clean(std::string event) {
+state_t cleaner_buy::clean(event_t event) {
     (*this).event = event;
+    return state;
 }
 
-std::string cleaner_buy::print() {
+event_t cleaner_buy::print() const {
     return event;
 }
 
 
-int main() {
-    cleaner_buy cb;
-    cleaner* cp = &cb;
-    cleaner_buy cleaner;
+BOOST_AUTO_TEST_SUITE(inheritance)
 
-    cleaner.clean("{\"name\": \"buy\"}");
-    std::cout << cleaner.print();
-    cp->clean("{\"name\": \"sell\"}");
-    std::cout << cp->print();
+BOOST_AUTO_TEST_CASE(virtual_override) {
+    const event_t event = R"<<<({"name": "buy"})<<<";
+    cleaner_buy cb;
+    BOOST_TEST("idle" == cb.clean(event));
 }
 
+BOOST_AUTO_TEST_CASE(virtual_override_on_base) {
+    const event_t event = R"<<<({"name": "sell"})<<<";
+    cleaner_buy cb;
+    cleaner* cp = &cb;
+    BOOST_TEST("idle" == cp->clean(event));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
